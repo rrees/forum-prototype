993f6ebbdcebcfb8869e67156216dbfb3cee4b2d
Robert Rees <shudderfix@gmail.com>
2024-12-04 21:08:39 +0000

    Creates my first model

--- illforum/forums/models.py
+++ illforum/forums/models.py
@@ -1,3 +1,20 @@
+import uuid
+
 from django.db import models
 
 # Create your models here.
+
+FORUM_TITLE_MAX = 300
+FORUM_POST_SUBJECT_MAX = 255
+
+
+class Forum(models.Model):
+    title = models.CharField(max_length=FORUM_TITLE_MAX)
+    external_id = models.UUIDField(default=uuid.uuid4, editable=False)
+
+
+class Post(models.Model):
+    forum = models.ForeignKey(Forum, on_delete=models.CASCADE)
+    external_id = models.UUIDField(default=uuid.uuid4, editable=False)
+    subject = models.CharField(max_length=FORUM_POST_SUBJECT_MAX, null=True)
+    content = models.TextField()
